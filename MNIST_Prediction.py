c86f66e326e08e352493f5e9822b0bff399cb1dc
Santhosh C P <Pok.santhosh@gmail.com>
2021-04-10 16:06:34 +0530

    Built KNN model using sklearn

--- MNIST_Prediction.py
+++ MNIST_Prediction.py
@@ -1,16 +1,16 @@
 import numpy as np
 import pandas as pd
+import random
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
-
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 def distance(p1, p2):
     return np.sum((p2 - p1) ** 2) ** 0.5
 
 
-def kNearestNeighbors(X, y, queryPoint, k=5):
+def manualKNN(X, y, queryPoint, k=5):
     m = X.shape[0]  # Here, it's 100
     distances = []
 
@@ -29,29 +29,49 @@ def kNearestNeighbors(X, y, queryPoint, k=5):
     return prediction
 
 
+def  displayAccuracy(X_train, y_train, X_test, y_test):
+    predictions = []
+    num = len(y_test)
+    correct = 0  # Number of correct predictions
 
+    for i in range(num):
+        prediction = manualKNN(X_train, y_train, X_test[i])
+        if int(prediction) == int(y_test[i]):
+            correct += 1
+        print(f"i={i} Correct={correct}")
+    print(f"Number of correct predictions = {correct}")
+
+    
+
+def libraryKNN(X_train, X_test, y_train, y_test):
+    knn = KNeighborsClassifier()
+    knn.fit(X_train, y_train)
+    predictions = knn.predict(X_test)
+    accuracyScore = accuracy_score(y_test, predictions)
+    print(f"Accuracy Score of KNN Model = {accuracyScore}")
+
+    return knn
+
+
+def randomSample(model, X_test, y_test):
+    num = len(y_test)
+
+    x = random.randint(0, num-1)
+    print(f"Random Value = {x}")
+    print(f"Predicted Value = {model.predict(x)[0]}")
+    plt.imshow(X_test.reshape(28, 28))
+    plt.show()
 
 def main():
+    # Reading the MNIST dataset
     data = pd.read_csv("train.csv")
+    print(data.shape)
     mnist = data.values
     X = mnist[:, :-1]
     y = mnist[:, -1]
 
-
-
-    # Splitting into train and test
-    X_train = X[0:15000]
-    y_train = y[0:15000]
-    X_test = X[15000:20000]
-    y_test = y[15000:20000]
-
-    randomNumber = 533
-
-    plt.imshow(X_test[randomNumber].reshape(28,28))
-    plt.show()
-    prediction = kNearestNeighbors(X_train, y_train, X_test[randomNumber])
-    print(f"Actual Output = {y_test[randomNumber]}")
-    print(f"Predictd Output = {prediction}")
-
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, random_state=50)
+    model = libraryKNN(X_train, X_test, y_train, y_test)
+    randomSample(model, X_test, y_test)
 
 main()
